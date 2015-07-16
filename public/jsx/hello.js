b77bd27d975bbc3eaff27f1208eedcc5f1ea310d
Bikash Kumar Sharma <bikash1999@yahoo.com>
2015-07-16 15:15:44 +0530

    some example

--- public/jsx/hello.js
+++ public/jsx/hello.js
@@ -2,3 +2,33 @@ React.render(
     React.DOM.h1(null, 'Hello, world!'),
     document.getElementById('example')
 );
+
+var HelloWorld = React.createClass({	
+	render: function() {
+	return React.createElement("div", null, "Hello World");
+  }
+});
+
+var InputTest = React.createClass({
+	getInitialState: function(){
+		return {
+			username: 'bikash'
+		}
+	},
+	handleChange: function(e){
+		this.setState({
+			username: e.target.value
+		});
+	},
+	render: function() {
+		return(
+			<div>
+				Hello {this.state.username} <br/>
+				Change name: <input type="text" value={this.state.username} onChange={this.handleChange}/>
+			</div>
+		)
+	}
+});
+
+React.render(<HelloWorld />, document.getElementById('example1'));
+React.render(<InputTest />, document.getElementById('InputTest'));
